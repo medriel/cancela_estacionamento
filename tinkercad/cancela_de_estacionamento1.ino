9bf26d86a3671b16ad4300f89d6c0b182cbe2bee
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-08-29 09:27:13 -0300

    first commit

---
+++ tinkercad/cancela_de_estacionamento1.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+
+Servo servo_9;
+const int pino_sensor(13);
+const int pino(12);
+int valor_sensor;
+
+void setup(){
+  Serial.begin(9600);
+  servo_9.attach(9, 500, 2500);
+  pinMode(pino_sensor,INPUT_PULLUP);
+  pinMode(pino, OUTPUT);
+}
+
+void tratar_fechar_cancela(){
+  delay(100);
+  
+  int cont_tempo = 0;
+
+  for(cont_tempo;cont_tempo<50;cont_tempo++){
+    valor_sensor = digitalRead(pino_sensor);
+    if(valor_sensor == HIGH){
+      Serial.println("Tem carro");
+      digitalWrite(pino,HIGH);
+      servo_9.write(90);
+      cont_tempo = 0;
+    }else if(valor_sensor == LOW){
+      Serial.println("Não tem carro");
+      cont_tempo++;
+      delay(100);
+    }
+  }
+  digitalWrite(pino,LOW);
+}
+
+void loop(){
+  
+  valor_sensor = digitalRead(pino_sensor);
+  
+    if(valor_sensor == HIGH){
+      Serial.println("Tem carro");
+      digitalWrite(pino,HIGH);
+      servo_9.write(90);
+      delay(100);
+      tratar_fechar_cancela();
+  	}
+  
+ 	if(valor_sensor == LOW){
+      Serial.println("Nao tem carro");
+      digitalWrite(pino,LOW);
+      servo_9.write(0);
+      delay(100);
+  	}
+}
