ab85bf162206e100b34372f450db6014ff8c4653
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-09-18 13:58:26 -0300

    Ajustando código do arduino

--- Arduino/cancela_estacionamento/cancela_estacionamento.ino
+++ Arduino/cancela_estacionamento/cancela_estacionamento.ino
@@ -6,7 +6,7 @@ const int pino(12);
 int valor_sensor;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(2000000);
   servo_9.attach(9, 500, 2500);
   pinMode(pino_sensor,INPUT_PULLUP);
   pinMode(pino, OUTPUT);
@@ -20,13 +20,13 @@ void tratar_fechar_cancela(){
   for(cont_tempo;cont_tempo<50;cont_tempo++){
     valor_sensor = digitalRead(pino_sensor);
     if(valor_sensor == HIGH){
-      Serial.println("Tem carro");
+      Serial.println("Aberto");
       digitalWrite(pino,HIGH);
       servo_9.write(90);
       cont_tempo = 0;
-    }else if(valor_sensor == LOW){
-      Serial.println("Não tem carro");
-      cont_tempo++;
+    }else 
+      if(valor_sensor == LOW){
+      Serial.println("Fechado");
       delay(100);
     }
   }
@@ -38,17 +38,17 @@ void loop(){
   valor_sensor = digitalRead(pino_sensor);
   
     if(valor_sensor == HIGH){
-      Serial.println("Tem carro");
+      Serial.println("Aberto");
       digitalWrite(pino,HIGH);
       servo_9.write(90);
       delay(100);
       tratar_fechar_cancela();
-  	}
+    }
   
- 	if(valor_sensor == LOW){
-      Serial.println("Nao tem carro");
+  if(valor_sensor == LOW){
+      Serial.println("Fechado");
       digitalWrite(pino,LOW);
       servo_9.write(0);
       delay(100);
-  	}
+    }
 }
